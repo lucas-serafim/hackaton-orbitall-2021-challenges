72b0cb099106eba611775261673f49f38fc78584
lucas-serafim <lucas.serafim159@gmail.com>
2021-01-23 15:12:32 -0300

    Node

--- desafio-final-hack21/nodejs/cards/src/controllers/card-controller.js
+++ desafio-final-hack21/nodejs/cards/src/controllers/card-controller.js
@@ -2,6 +2,62 @@ const neDB = require('../configurations/database')
 const api = {}
 
 api.findAll = (request, response) => {
+   neDB.find({}).exec((exception, cards) => {
+      if (exception) {
+         response.status(exception.status | 400)
+         response.json({ 'Message': "Error"})
+      }
+      response.status(200)
+      response.json(cards)
+   })
+}
+
+api.insert = (request, response) => {
+   const canonical = request.body
+
+   neDB.insert(canonical, (exception, cards) => {
+      if (exception) {
+         response.status(exception.status | 400)
+         response.json({ 'Message': "Error"})
+      }
+
+      response.status(201)
+      response.json(cards)
+   })
+}
+
+api.update = (request, response) => {
+   neDB.update({ _id: request.params.id }, (exception, cards) => {
+      if (exception) {
+         response.status(exception.status | 400)
+         response.json({ 'Message': "Error"})
+      }
+
+      response.status(200)
+   })
+}
+
+api.remove = (request, response) => {
+   neDB.remove({  _id: request.params.id }, (exception, cards) => {
+      if (exception) {
+         response.status(exception.status | 400)
+         response.json({ 'Message': "Error"})
+      }
+
+      response.status(200)
+   })
+}
+
+api.findById = (request, response) => {
+   neDB.findOne({ _id: request.params.id }, (exception, cards) => {
+      if (exception) {
+         response.status(exception.status | 400)
+         response.json({ 'Message': "Error"})
+      }
+
+      response.status(200)
+      response.json(cards)
+   })
 }
 
 module.exports = api
--- desafio-final-hack21/nodejs/cards/src/routes/card-route.js
+++ desafio-final-hack21/nodejs/cards/src/routes/card-route.js
@@ -1,7 +1,12 @@
 const api = require('../controllers/card-controller')
 
 module.exports = (app) => {
-    app.route('')
+    app.route('/cards')
         .get(api.findAll)
+        .post(api.insert)
 
+   app.route('/:id')
+         .put(api.update)
+         .delete(api.remove)
+         .get(api.findById)
 }
