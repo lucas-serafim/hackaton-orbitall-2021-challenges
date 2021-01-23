447e6165c721bc036d4a1cc993647bc689653947
lucas-serafim <lucas.serafim159@gmail.com>
2021-01-23 09:51:07 -0300

    Entity Card Java

---
+++ desafio-final-hack21/java/cards/src/main/java/com/orbitallcorp/hack21/cards/domains/Card.java
@@ -0,0 +1,130 @@
+package com.orbitallcorp.hack21.cards.domains;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_card")
+public class Card implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String cardNumber;
+	private String embossName;
+	private String customerName;
+	private String documentNumber;
+	private String motherName;
+	private String address;
+	private String city;
+	
+	public Card() {
+	}
+	
+	public Card(Long id, String cardNumber, String embossName, String customerName, String documentNumber,String motherName, String address, String city) {
+		this.id = id;
+		this.cardNumber = cardNumber;
+		this.embossName = embossName;
+		this.customerName = customerName;
+		this.documentNumber = documentNumber;
+		this.motherName = motherName;
+		this.address = address;
+		this.city = city;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCardNumber() {
+		return cardNumber;
+	}
+
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	public String getEmbossName() {
+		return embossName;
+	}
+
+	public void setEmbossName(String embossName) {
+		this.embossName = embossName;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getDocumentNumber() {
+		return documentNumber;
+	}
+
+	public void setDocumentNumber(String documentNumber) {
+		this.documentNumber = documentNumber;
+	}
+
+	public String getMotherName() {
+		return motherName;
+	}
+
+	public void setMotherName(String motherName) {
+		this.motherName = motherName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Card other = (Card) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
