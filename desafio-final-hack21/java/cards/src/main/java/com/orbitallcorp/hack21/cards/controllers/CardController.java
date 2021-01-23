b70d4f9c1ce1f2e43582bbeccdfafbe427c2a69c
lucas-serafim <lucas.serafim159@gmail.com>
2021-01-23 11:07:33 -0300

    Update and delete

--- desafio-final-hack21/java/cards/src/main/java/com/orbitallcorp/hack21/cards/controllers/CardController.java
+++ desafio-final-hack21/java/cards/src/main/java/com/orbitallcorp/hack21/cards/controllers/CardController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +33,17 @@ public class CardController {
 	public ResponseEntity<CardDTO> insert(@RequestBody CardDTO dto){
 		dto = cardService.insert(dto);
 		return ResponseEntity.status(201).body(dto);
+	}
+	
+	@PutMapping
+	public ResponseEntity<CardDTO> update(@PathVariable Long id, @RequestBody CardDTO dto){
+		dto = cardService.update(id, dto);
+		return ResponseEntity.status(200).body(dto);
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		cardService.delete(id);
+		return ResponseEntity.status(200).build();
 	}
 }
--- desafio-final-hack21/java/cards/src/main/java/com/orbitallcorp/hack21/cards/service/CardService.java
+++ desafio-final-hack21/java/cards/src/main/java/com/orbitallcorp/hack21/cards/service/CardService.java
@@ -1,6 +1,7 @@
 package com.orbitallcorp.hack21.cards.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,5 +29,29 @@ public class CardService {
 		Card card = new Card(null, dto.getCardNumber(), dto.getEmbossName(), dto.getCustomerName(), dto.getDocumentNumber(), dto.getMotherName(), dto.getAddress(), dto.getCity());
 		card = cardRepository.save(card);
 		return new CardDTO(card);
+	}
+	
+	@Transactional
+	public CardDTO update(Long id, CardDTO dto) {
+		Optional<Card> card = cardRepository.findById(id);
+		updateData(card, dto);
+		
+		return new CardDTO(card.get());
+	}
+	
+	@Transactional
+	public void delete(Long id) {
+		Optional<Card> card = cardRepository.findById(id);
+		cardRepository.deleteById(card.get().getId());
+	}
+	
+	private void updateData(Optional<Card> card, CardDTO dto) {
+		card.get().setCardNumber(dto.getCardNumber());
+		card.get().setEmbossName(dto.getEmbossName());
+		card.get().setCustomerName(dto.getCustomerName());
+		card.get().setDocumentNumber(dto.getDocumentNumber());
+		card.get().setMotherName(dto.getMotherName());
+		card.get().setAddress(dto.getAddress());
+		card.get().setCity(dto.getCity());
 	}
 }
